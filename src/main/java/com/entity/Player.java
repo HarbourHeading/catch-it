5eadd3829b46171860914f38fcf84e81b1091224
Liam Ek <Liam.a.ek05@gmail.com>
2024-04-08 21:26:27 +0200

    Added user/player class

---
+++ src/main/java/com/entity/Player.java
@@ -0,0 +1,53 @@
+package com.entity;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+import com.app.GamePanel;
+import com.app.KeyListen;
+
+public class Player extends Entity {
+
+	GamePanel gamePanel;
+	KeyListen keyListen;
+	int leftBorder = 0;
+
+	public Player(GamePanel gamePanel, KeyListen keyListen) {
+
+		this.gamePanel = gamePanel;
+		this.keyListen = keyListen;
+
+		setDefaultValues();
+	}
+
+	public void setDefaultValues() {
+
+		x = 100;
+		y = 500;
+		velocity = 4;
+	}
+
+	public void update() {
+
+		if (keyListen.leftPressed == true) {
+			x -= velocity;
+			if (x < leftBorder) {
+				x = leftBorder;
+			}
+
+		}
+
+		else if (keyListen.rightPressed == true) {
+			x += velocity;
+			if (x > gamePanel.screenWidth - gamePanel.tileSize) {
+				x = gamePanel.screenWidth - gamePanel.tileSize;
+			}
+		}
+	}
+
+	public void draw(Graphics2D g2D) {
+
+		g2D.setColor(Color.white);
+		g2D.fillRect(x, y, gamePanel.tileSize, gamePanel.tileSize);
+	}
+}
