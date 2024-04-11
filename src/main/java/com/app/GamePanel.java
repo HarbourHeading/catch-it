29166a909c09818e199c736bb0ffa2bea46a98e8
Liam Ek <Liam.a.ek05@gmail.com>
2024-04-11 22:02:23 +0200

    # GamePanel - Added Ui object - Added finishState - Added score counter - Added JOptionPane ERROR_MESSAGE on InterruptedException - Added ui draw method call - Changed screen variable double

--- src/main/java/com/app/GamePanel.java
+++ src/main/java/com/app/GamePanel.java
@@ -10,6 +10,15 @@ import javax.swing.JPanel;
 import com.entity.Enemy;
 import com.entity.Player;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JPanel;
+import javax.swing.JOptionPane;
+
+
 public class GamePanel extends JPanel implements Runnable {
 	private static final long serialVersionUID = -7037762034212883804L;
 
@@ -17,11 +26,11 @@ public class GamePanel extends JPanel implements Runnable {
 	final int originalTileSize = 16;
 	final int upScale = 3;
 	public final int tileSize = originalTileSize * upScale;
-	
+
 	final int maxScreenCol = 16;
 	final int maxScreenRow = 12;
-	public final int screenWidth = tileSize * maxScreenCol; // REMIND: Constant -> lower speed the higher screen width
-	public final int screenHeight = tileSize * maxScreenRow; // REMIND: Constant -> higher speed the higher screen height
+	public final int screenWidth = tileSize * maxScreenCol; // FIXME: Constant -> lower speed the higher screen width
+	public final int screenHeight = tileSize * maxScreenRow; // FIXME: Constant -> higher speed the higher screen height
 
 	int fps = 60; // FPS (Frames per second)
 
@@ -30,21 +39,23 @@ public class GamePanel extends JPanel implements Runnable {
 	public Player player = new Player(this, keyHandler);
 	public Enemy enemy = new Enemy(this);
 	public CollisionDetection collisionDetection = new CollisionDetection(this);
+	Ui ui = new Ui(this);
 
-	// Game State
+	// Game States
 	public int gameState;
-	public final int playState = 1;
 	public final int pauseState = 0;
-	
-	// Health
+	public final int playState = 1;
+	public final int finishState = 2;
+
+	// In-game values
 	public int health = 5;
+	public int score = 0;
 
 	public GamePanel() {
 
 		this.setPreferredSize(new Dimension(screenWidth, screenHeight));
 		this.setBackground(Color.black);
 		this.setDoubleBuffered(true);
-
 		this.addKeyListener(keyHandler);
 		this.setFocusable(true);
 	}
@@ -62,7 +73,7 @@ public class GamePanel extends JPanel implements Runnable {
 	@Override
 	public void run() {
 
-		double drawInterval = 1000000000 / fps; // 1 second == 1 billion nanoseconds / fps (drawInterval = 0.016667s)
+		double drawInterval = 1000000000d / fps; // 1 second == 1 billion nanoseconds / fps (drawInterval = 0.016667s)
 		double nextDrawTime = System.nanoTime() + drawInterval;
 
 		while (gameThread != null) {
@@ -78,7 +89,10 @@ public class GamePanel extends JPanel implements Runnable {
 				Thread.sleep((long) remainingTime);
 
 			} catch (InterruptedException e) {
-				e.printStackTrace();
+				JOptionPane.showMessageDialog(this,
+						"An error occurred!",
+						"InterruptedException", JOptionPane.ERROR_MESSAGE);
+				System.exit(1);
 			}
 		}
 	}
@@ -89,15 +103,16 @@ public class GamePanel extends JPanel implements Runnable {
 			player.update();
 			enemy.update();
 		}
-		// REMIND: Add score addition on collision. Return enemy to x = randint(gamePanel.screenWidth)
 	}
 
 	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
 
+		super.paintComponent(g);
 		Graphics2D g2D = (Graphics2D) g;
+
 		player.draw(g2D);
 		enemy.draw(g2D);
+		ui.draw(g2D);
 
 		g2D.dispose();
 	}
