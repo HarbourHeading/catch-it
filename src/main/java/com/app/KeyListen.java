eca0505d278cc6e558791f2d321171ac11e201e0
Liam Ek <Liam.a.ek05@gmail.com>
2024-04-16 20:32:43 +0200

    Added F key event listener to reset game. Changed variable scopes.

--- src/main/java/com/app/KeyListen.java
+++ src/main/java/com/app/KeyListen.java
@@ -5,53 +5,53 @@ import java.awt.event.KeyListener;
 
 public class KeyListen implements KeyListener {
 
-	public boolean leftPressed, rightPressed;
-	GamePanel gamePanel;
+    final GamePanel gamePanel;
+    public boolean leftPressed, rightPressed;
 
-	public KeyListen(GamePanel gamePanel) {
-		this.gamePanel = gamePanel;
-	}
+    public KeyListen(GamePanel gamePanel) {
+        this.gamePanel = gamePanel;
+    }
 
-	@Override
-	public void keyPressed(KeyEvent e) {
-		int keyPress = e.getKeyCode();
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int keyPress = e.getKeyCode();
 
-		if (keyPress == KeyEvent.VK_LEFT || keyPress == KeyEvent.VK_A) {
-			leftPressed = true;
-		}
-		if (keyPress == KeyEvent.VK_RIGHT || keyPress == KeyEvent.VK_D) {
-			rightPressed = true;
-		}
-		if (keyPress == KeyEvent.VK_ESCAPE) {
-			if (gamePanel.gameState == gamePanel.playState) {
-				gamePanel.gameState = gamePanel.pauseState;
-				
-			} else if (gamePanel.gameState == gamePanel.pauseState) {
-				gamePanel.gameState = gamePanel.playState;
-			}
-		}
-	}
+        if (keyPress == KeyEvent.VK_LEFT || keyPress == KeyEvent.VK_A) {
+            leftPressed = true;
+        }
+        if (keyPress == KeyEvent.VK_RIGHT || keyPress == KeyEvent.VK_D) {
+            rightPressed = true;
+        }
+        if (keyPress == KeyEvent.VK_ESCAPE) {
+            if (gamePanel.gameState == gamePanel.playState) {
+                gamePanel.gameState = gamePanel.pauseState;
 
-	@Override
-	public void keyReleased(KeyEvent e) {
-		int keyPress = e.getKeyCode();
+            } else if (gamePanel.gameState == gamePanel.pauseState) {
+                gamePanel.gameState = gamePanel.playState;
+            }
+        }
+        if (keyPress == KeyEvent.VK_F) {
+            gamePanel.health = 5;
+            gamePanel.score = 0;
+            gamePanel.gameState = gamePanel.pauseState;
+            gamePanel.leaderboardCalculated = false;
+        }
+    }
 
-		if (keyPress == KeyEvent.VK_LEFT || keyPress == KeyEvent.VK_A) {
-			leftPressed = false;
-		}
-		if (keyPress == KeyEvent.VK_RIGHT || keyPress == KeyEvent.VK_D) {
-			rightPressed = false;
-		}
+    @Override
+    public void keyReleased(KeyEvent e) {
+        int keyPress = e.getKeyCode();
 
-	}
+        if (keyPress == KeyEvent.VK_LEFT || keyPress == KeyEvent.VK_A) {
+            leftPressed = false;
+        }
+        if (keyPress == KeyEvent.VK_RIGHT || keyPress == KeyEvent.VK_D) {
+            rightPressed = false;
+        }
+    }
 
-	/**
-	 * Unused method. Returns.
-	 * 
-	 * @hidden
-	 */
-	@Override
-	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
-	}
+    //	 Unused method.
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
 }
