760c6987d97bbe4da07b8e4eb6966cb16c53fd27
Liam Ek <Liam.a.ek05@gmail.com>
2024-04-20 14:36:12 +0200

    Updated Main description of project to README.

--- src/main/java/com/app/Main.java
+++ src/main/java/com/app/Main.java
@@ -9,7 +9,8 @@ import javax.swing.JFrame;
  *
  * The game starts in a paused state. Press the ESC key to unpause. Use A or left arrow to move left,
  * and D or right arrow to move right. Upon losing all 5 lives, you are greeted with a game over screen.
- * The game over screen displays the top 3 scores. If your score beats one of them, it replaces the lower score permanently.
+ * The game over screen displays the top 3 scores. If your score beats one of them, it replaces the lower score.
+ * Press F to restart.
  *
  * @author Liam
  * @since 1.0
