8627d99e466704c3d1143dc5081161b012f9a126
Liam Ek <Liam.a.ek05@gmail.com>
2024-04-20 14:35:46 +0200

    Improved readability.

--- src/main/java/com/app/KeyListen.java
+++ src/main/java/com/app/KeyListen.java
@@ -14,25 +14,26 @@ public class KeyListen implements KeyListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
+
         int keyPress = e.getKeyCode();
 
-        if (keyPress == KeyEvent.VK_LEFT || keyPress == KeyEvent.VK_A) {
-            leftPressed = true;
-        }
-        if (keyPress == KeyEvent.VK_RIGHT || keyPress == KeyEvent.VK_D) {
-            rightPressed = true;
-        }
+        if (keyPress == KeyEvent.VK_LEFT || keyPress == KeyEvent.VK_A) { leftPressed = true; }
+        if (keyPress == KeyEvent.VK_RIGHT || keyPress == KeyEvent.VK_D) { rightPressed = true; }
+
         if (keyPress == KeyEvent.VK_ESCAPE) {
             if (gamePanel.gameState == gamePanel.playState) {
                 gamePanel.gameState = gamePanel.pauseState;
+            }
 
-            } else if (gamePanel.gameState == gamePanel.pauseState) {
+            else if (gamePanel.gameState == gamePanel.pauseState) {
                 gamePanel.gameState = gamePanel.playState;
             }
         }
+
         if (keyPress == KeyEvent.VK_F) {
             gamePanel.health = 5;
             gamePanel.score = 0;
+
             gamePanel.gameState = gamePanel.pauseState;
             gamePanel.leaderboardCalculated = false;
         }
@@ -40,17 +41,14 @@ public class KeyListen implements KeyListener {
 
     @Override
     public void keyReleased(KeyEvent e) {
+
         int keyPress = e.getKeyCode();
 
-        if (keyPress == KeyEvent.VK_LEFT || keyPress == KeyEvent.VK_A) {
-            leftPressed = false;
-        }
-        if (keyPress == KeyEvent.VK_RIGHT || keyPress == KeyEvent.VK_D) {
-            rightPressed = false;
-        }
+        if (keyPress == KeyEvent.VK_LEFT || keyPress == KeyEvent.VK_A) { leftPressed = false; }
+        if (keyPress == KeyEvent.VK_RIGHT || keyPress == KeyEvent.VK_D) { rightPressed = false; }
     }
 
-    //	 Unused method.
+    // Unused method.
     @Override
     public void keyTyped(KeyEvent e) {
     }
