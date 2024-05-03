8959cfa90eacdde287bb43b694cb67302f71137e
Liam Ek <Liam.a.ek05@gmail.com>
2024-05-03 18:43:43 +0200

    Removed gamePanel.setupGame method call. The function's use is moved to the GamePanel constructor. Changed the window's name to better fit the GitHub name.

--- src/main/java/com/app/Main.java
+++ src/main/java/com/app/Main.java
@@ -22,17 +22,15 @@ public class Main {
 		JFrame window = new JFrame();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setResizable(false);
-		window.setTitle("Catch - GAME");
+		window.setTitle("Catch it");
 
 		GamePanel gamePanel = new GamePanel();
 		window.add(gamePanel);
-
 		window.pack();
 
 		window.setLocationRelativeTo(null);
 		window.setVisible(true);
 
-		gamePanel.setupGame();
 		gamePanel.startGameThread();
 	}
 }
