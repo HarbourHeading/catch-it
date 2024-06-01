80d3a532285688b3a936962268f73f9c95ccd7e5
Liam Ek <Liam.a.ek05@gmail.com>
2024-06-01 21:02:45 +0200

    Added Player sprite. Changed player spawn on x-axis: 0.8 -> 0.7 x screenHeight.

--- src/main/java/com/entity/Player.java
+++ src/main/java/com/entity/Player.java
@@ -3,19 +3,22 @@ package com.entity;
 import com.app.GamePanel;
 import com.app.KeyListen;
 
+import javax.swing.ImageIcon;
 import java.awt.Graphics2D;
-import java.awt.Color;
+import java.util.Objects;
 
 public class Player extends Entity {
 
 	final GamePanel gamePanel;
 	final KeyListen keyListen;
 	final int leftBorder = 0;
+	private final ImageIcon playerSprite;
 
 	public Player(GamePanel gamePanel, KeyListen keyListen) {
 
 		this.gamePanel = gamePanel;
 		this.keyListen = keyListen;
+		playerSprite = new ImageIcon(Objects.requireNonNull(getClass().getResource("/player.png")));
 
 		this.setDefaultValues();
 	}
@@ -23,7 +26,7 @@ public class Player extends Entity {
 	public void setDefaultValues() {
 
 		x = gamePanel.screenWidth / 2 - gamePanel.tileSize / 2; // Center screen
-		y = (int) (gamePanel.screenHeight * 0.80d);
+		y = (int) (gamePanel.screenHeight * 0.70d);
 		velocity = 4;
 	}
 
@@ -46,13 +49,12 @@ public class Player extends Entity {
 		}
 
 		gamePanel.enemy.intersect = false;
-		gamePanel.collisionDetection.checkTile();
+		gamePanel.collisionDetection.checkTileEnemy();
 	}
 
 	public void draw(Graphics2D g2D) {
 
-		g2D.setColor(Color.white);
-		g2D.fillRect(x, y, gamePanel.tileSize, gamePanel.tileSize);
+		g2D.drawImage(playerSprite.getImage(), x, y, gamePanel.tileSize, gamePanel.tileSize, gamePanel);
 	}
 
 }
