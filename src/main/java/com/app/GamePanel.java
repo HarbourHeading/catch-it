0e9a2c3ba5106ed3c7ff72bb91ebf6abf3a3b562
Liam Ek <Liam.a.ek05@gmail.com>
2024-06-01 21:08:00 +0200

    Added Background image. Improved comment spacing.

--- src/main/java/com/app/GamePanel.java
+++ src/main/java/com/app/GamePanel.java
@@ -3,13 +3,12 @@ package com.app;
 import com.entity.Enemy;
 import com.entity.Player;
 
-import javax.swing.JPanel;
-import javax.swing.JOptionPane;
+import javax.swing.*;
 
 import java.awt.Dimension;
-import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.util.Objects;
 
 
 public class GamePanel extends JPanel implements Runnable {
@@ -24,16 +23,17 @@ public class GamePanel extends JPanel implements Runnable {
 	final int maxScreenRow = 12;
 	public final int screenWidth = tileSize * maxScreenCol;
 	public final int screenHeight = tileSize * maxScreenRow;
+	final int fps = 60;  // FPS (Frames per second)
 
-	Thread gameThread;
+	private Thread gameThread;
 	public final CollisionDetection collisionDetection = new CollisionDetection(this);
-	final int fps = 60; // FPS (Frames per second)
-	final KeyListen keyHandler = new KeyListen(this);
+	private final KeyListen keyHandler = new KeyListen(this);
 	public final Player player = new Player(this, keyHandler);
-	final Ui ui = new Ui(this);
+	private final Ui ui = new Ui(this);
 	public final Enemy enemy = new Enemy(this);
+	private final ImageIcon backgroundImage;
 
-	// Game States
+    // Game States
 	public int gameState;
 	public final int pauseState = 0;
 	public final int playState = 1;
@@ -47,7 +47,7 @@ public class GamePanel extends JPanel implements Runnable {
 	public GamePanel() {
 
 		this.setPreferredSize(new Dimension(screenWidth, screenHeight));
-		this.setBackground(Color.black);
+		backgroundImage = new ImageIcon(Objects.requireNonNull(getClass().getResource("/background.png")));
 		this.setDoubleBuffered(true);
 		this.addKeyListener(keyHandler);
 		this.setFocusable(true);
@@ -63,7 +63,7 @@ public class GamePanel extends JPanel implements Runnable {
 	@Override
 	public void run() {
 
-		double drawInterval = 1000000000d / fps; // 1 second == 1 billion nanoseconds / fps (drawInterval = 0.016667s)
+		double drawInterval = 1000000000d / fps;  // 1 second == 1 billion nanoseconds / fps (drawInterval = 0.016667s)
 		double nextDrawTime = System.nanoTime() + drawInterval;
 
 		while (gameThread != null) {
@@ -76,7 +76,7 @@ public class GamePanel extends JPanel implements Runnable {
 				double remainingTime = nextDrawTime - System.nanoTime();
 				remainingTime = remainingTime / 1000000;
 				nextDrawTime += drawInterval;
-				Thread.sleep((long) Math.max(0, remainingTime)); // In case of exception where remainingTime < 0: take max where 0 is lowest
+				Thread.sleep((long) Math.max(0, remainingTime));  // In case of exception where remainingTime < 0: take max where 0 is lowest
 
 			} catch (InterruptedException e) {
 				JOptionPane.showMessageDialog(this,
@@ -106,6 +106,9 @@ public class GamePanel extends JPanel implements Runnable {
 
 		super.paintComponent(g);
 		Graphics2D g2D = (Graphics2D) g;
+
+		g2D.drawImage(backgroundImage.getImage(), 0, 0, screenWidth, screenHeight, this);
+
 
 		player.draw(g2D);
 		enemy.draw(g2D);
