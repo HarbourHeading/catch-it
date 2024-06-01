29ed75cb55ef8629ed1cd94fe8e3e8d1c9872147
Liam Ek <Liam.a.ek05@gmail.com>
2024-06-01 21:09:09 +0200

    Changed: checkTile -> checkTileEnemy for clarity.

--- src/main/java/com/app/CollisionDetection.java
+++ src/main/java/com/app/CollisionDetection.java
@@ -8,7 +8,7 @@ public class CollisionDetection {
 		this.gamePanel = gamePanel;
 	}
 
-	public void checkTile() {
+	public void checkTileEnemy() {
 		
 		if (gamePanel.player.x < (gamePanel.enemy.x + gamePanel.tileSize) && gamePanel.player.y < (gamePanel.enemy.y + gamePanel.tileSize) 
 		&& (gamePanel.player.x + gamePanel.tileSize) > gamePanel.enemy.x && (gamePanel.player.y + gamePanel.tileSize) > gamePanel.enemy.y) {
